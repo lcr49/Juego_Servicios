7448ccaed39e33133fc9310f0c35c9c117d833db
lcr49 <lauriss9494@gmail.com>
2016-10-24 16:39:29 +0200

    Galleta posicionada

--- lauritaGame.py
+++ lauritaGame.py
@@ -7,7 +7,15 @@ from pygame.locals import *
 ancho = 640
 alto = 480
 
- 
+class Galleta(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = cargarImagen("imagenes/galleta.png", True)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = 30
+        self.rect.centery = 360
+        self.speed = [0.5, -0.5]
+         
 def cargarImagen(filename, transparent=False):  
     image = pygame.image.load(filename)       
     return image.convert_alpha()
@@ -15,15 +23,17 @@ def cargarImagen(filename, transparent=False):
 def main():
     screen = pygame.display.set_mode((ancho, alto))
     pygame.display.set_caption("Laurita Game")
- 
     background_image = cargarImagen('imagenes/fondo3.png')
+    #creamos objetos
+    galleta = Galleta()
  
     while True:
         for eventos in pygame.event.get():
             if eventos.type == QUIT:
                 sys.exit(0)
- 
+        #ponemos en ventana
         screen.blit(background_image, (0, 0))
+        screen.blit(galleta.image, galleta.rect)
         pygame.display.flip()
     return 0
  
