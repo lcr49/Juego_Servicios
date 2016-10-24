eb93ab4b809fbc349757a80ba0500b444fa070f3
lcr49 <lauriss9494@gmail.com>
2016-10-24 19:51:52 +0200

    Trofeo y Ganar

--- lauritaGame.py
+++ lauritaGame.py
@@ -4,9 +4,38 @@ from pygame.locals import *
  
 alto = 640
 ancho = 480
-def GameOver(self):
+class Trofeo(pygame.sprite.Sprite):
+    def __init__(self):#inicializa la clase
+        pygame.sprite.Sprite.__init__(self)
+        self.image = cargarImagen("imagenes/trofeo.png")#imagen muñeco
+        self.rect = self.image.get_rect()#dimensiones de la imagen
+        self.rect.centerx = 610
+        self.rect.centery = 376 #posicion el muñeco en la imagen
+        self.speed = 0.5#velocidad con la que se mueve el muñeco
+        
+ 
+def gameOver(self):
     screen = pygame.display.set_mode((alto, ancho)) 
     background_image = cargarImagen('imagenes/gameOver.png')
+    pygame.display.set_caption("Laurita Game")
+ 
+    clock = pygame.time.Clock()
+ 
+    while True:
+        time = clock.tick(60)
+        keys = pygame.key.get_pressed()
+        for eventos in pygame.event.get():
+            if eventos.type == QUIT:
+                sys.exit(0)
+        
+        screen.blit(background_image, (0, 0))
+
+        pygame.display.flip()
+    return 0 
+
+def ganar(self):
+    screen = pygame.display.set_mode((alto, ancho)) 
+    background_image = cargarImagen('imagenes/ganar.png')
     pygame.display.set_caption("Laurita Game")
  
     clock = pygame.time.Clock()
@@ -42,7 +71,7 @@ class Caramelo(pygame.sprite.Sprite):
         if pygame.sprite.collide_rect(self, galleta):
             self.speed[0] = -self.speed[0]
             self.rect.centerx += self.speed[1] * time
-            GameOver(self) 
+            gameOver(self) 
    
 
 class Galleta(pygame.sprite.Sprite):
@@ -62,6 +91,10 @@ class Galleta(pygame.sprite.Sprite):
             if keys[K_RIGHT]:#si tienes la tecla derecha pulsada se mueve
                 self.rect.centerx += self.speed * time
                 
+    def actualizarGalleta(self, time, trofeo):
+        if pygame.sprite.collide_rect(self, trofeo):
+            ganar(self)             
+                
 
 def cargarImagen(filename, transparent=False):  
     image = pygame.image.load(filename)       
@@ -72,6 +105,7 @@ def main():
     background_image = cargarImagen('imagenes/fondo3.png')
     pygame.display.set_caption("Laurita Game")
     galleta = Galleta()
+    trofeo = Trofeo()
     caramelo = Caramelo(1.05,1.99)
     caramelo1 = Caramelo(1.20,2.99)
     caramelo3 = Caramelo(1.50,9.99)
@@ -88,6 +122,7 @@ def main():
                 sys.exit(0)
         
         galleta.mover(time, keys)
+        galleta.actualizarGalleta(time, trofeo)
         caramelo.actualizar(time, galleta)
         caramelo1.actualizar(time, galleta)
         caramelo3.actualizar(time, galleta)
@@ -96,6 +131,7 @@ def main():
          
         screen.blit(background_image, (0, 0))
         screen.blit(galleta.image, galleta.rect)
+        screen.blit(trofeo.image, trofeo.rect)
         screen.blit(caramelo.image, caramelo.rect)
         screen.blit(caramelo1.image, caramelo1.rect)
         screen.blit(caramelo3.image, caramelo3.rect)
